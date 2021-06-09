cb991876291484f05286547a7dcbd0ab7269784a
lenarsharipov <lenarsharipov@gmail.com>
2021-06-09 09:06:42 +0300

    4.1.2. Глупый бот [#31323]

---
+++ src/ru/job4j/condition/DummyBot.java
@@ -0,0 +1,22 @@
+package ru.job4j.condition;
+
+public class DummyBot {
+
+    public static String answer(String question) {
+        String rsl = "Это ставит меня в тупик. Задайте другой вопрос.";
+        if ("Привет, Бот.".equals(question)) {
+            return "Привет, умник.";
+
+        } else if ("Пока.".equals(question)) {
+            return "До скорой встречи.";
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        String rsl = DummyBot.answer("Привет, Бот");
+        System.out.println(rsl);
+        rsl = DummyBot.answer("Пока.");
+        System.out.println(rsl);
+    }
+}
