0602ab9622a58763d9b2bdeca11d4a328ed82300
lenarsharipov <lenarsharipov@gmail.com>
2021-06-13 09:12:36 +0300

    5.7. Ипотека [#156315 #167023]

--- src/ru/job4j/loop/Mortgage.java
+++ src/ru/job4j/loop/Mortgage.java
@@ -3,9 +3,8 @@ package ru.job4j.loop;
 public class Mortgage {
     public static int year(double amount, int salary, double percent) {
         int year = 0;
-        double debt = amount + amount * percent / 100;
-        while (debt >= 0) {
-            debt = debt - salary;
+        while (amount > 0) {
+            amount = amount + amount * percent / 100 - salary;
             year++;
         }
         return year;
