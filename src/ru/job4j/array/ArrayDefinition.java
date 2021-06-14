1c9c962633ae2d2416799563d66d1df4a7fc4e28
lenarsharipov <lenarsharipov@gmail.com>
2021-06-14 11:07:20 +0300

    6.0.2. Заполнение массива. [#226300]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -9,5 +9,17 @@ public class ArrayDefinition {
         System.out.println(ages.length);
         System.out.println(surnames.length);
         System.out.println(prices.length);
+
+        String[] names = new String[4];
+        names[0] = "Jennifer Aniston";
+        names[1] = "Courtney Cox";
+        names[2] = "Lisa Kudrow";
+        names[3] = "Matt LeBlanc";
+
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
+
     }
 }
