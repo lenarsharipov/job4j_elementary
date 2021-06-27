359b324d7eb2f0292e0e4bbd715abcd62fcff2f5
lenarsharipov <lenarsharipov@gmail.com>
2021-06-27 18:25:50 +0300

    EXERCISE-ARRAYS. 31. Конвертировать неквадратный двумерный массив в квадратный. [#1.1.6.31]

--- src/ru/job4j/array/exercise/Converter.java
+++ src/ru/job4j/array/exercise/Converter.java
@@ -4,28 +4,22 @@ import java.util.Arrays;
 
 public class Converter {
     public static int[][] convertInSquareArray(int[][] array) {
-        /* Count array values quantity */
         int length = 0;
-        for (int i = 0; i < array.length; i++) {
+        for (int i = 0; i < array.length; i++) {    /* Count array values quantity */
             length += array[i].length;
         }
         length = (int) Math.ceil(Math.sqrt(length));
         int[] arrayTemp = new int[length * length];
         int count = 0;
-        /* Copying values of two-dimensional array to one-dimensional array */
-        for (int i = 0; i < array.length; i++) {
+        for (int i = 0; i < array.length; i++) {        /* Copying values of two-dimensional array to one-dimensional array */
             for (int j = 0; j < array[i].length; j++) {
                 arrayTemp[count] = array[i][j];
                 count++;
             }
         }
-        /* Copying values of one-dimensional array to newly created two-dimensional square matrix */
         int[][] rsl = new int[length][];
         count = 0;
-        for (int i = 0; i < rsl.length; i++) {
-            if (count == arrayTemp.length) {
-                break;
-            }
+        for (int i = 0; i < rsl.length; i++) {      /* Copying values of one-dimensional array to newly created two-dimensional square matrix */
             rsl[i] = Arrays.copyOfRange(arrayTemp, count, count + length);
             count += length;
         }
