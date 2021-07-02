b2c8d058bb8cabf9459174e9ba457467e91e6500
lenarsharipov <lenarsharipov@gmail.com>
2021-07-02 20:28:53 +0300

    EXERCISE-ARRAYS. 5.5. Циклический сдвиг вправо [#1.1.6.5.5]

---
+++ src/ru/job4j/array/exercise/RightShift.java
@@ -0,0 +1,22 @@
+package ru.job4j.array.exercise;
+
+public class RightShift {
+    public static void shift(int[] nums, int count) {
+        while (count > 0) {
+            RightShift.shift(nums);
+            count--;
+        }
+    }
+
+    // метод делает сдвиг с шагом 1
+    private static void shift(int[] nums) {
+        int[] array = new int[nums.length];
+        array[0] = nums[nums.length - 1];
+        for (int index = 1; index < array.length; index++) {
+            array[index] = nums[index - 1];
+        }
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = array[i];
+        }
+    }
+}
