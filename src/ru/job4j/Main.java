70eae193f2accb1c032cb7ed453051b40833e7b2
lenarsharipov <lenarsharipov@gmail.com>
2022-06-20 00:19:17 +0300

    3.2. Загрузка файлов на github [#243562 #164178]

--- src/ru/job4j/Main.java
+++ src/ru/job4j/Main.java
@@ -3,6 +3,6 @@ package ru.job4j;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
-        System.out.println("9th of May, 2022");
+        System.out.println("20th of June, 2022");
     }
 }
