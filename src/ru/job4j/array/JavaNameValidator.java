9a73c90176a353a60f11a2bec1ae5085b5c382bb
lenarsharipov <lenarsharipov@gmail.com>
2022-08-01 15:53:36 +0300

    6.9. String - это массив [#504884 #327674]

--- src/ru/job4j/array/JavaNameValidator.java
+++ src/ru/job4j/array/JavaNameValidator.java
@@ -20,4 +20,5 @@ public class JavaNameValidator {
         }
         return valid;
     }
+
 }
--- test/ru/job4j/array/JavaNameValidatorTest.java
+++ test/ru/job4j/array/JavaNameValidatorTest.java
@@ -6,7 +6,6 @@ import static org.assertj.core.api.Assertions.*;
 import static ru.job4j.array.JavaNameValidator.*;
 
 class JavaNameValidatorTest {
-
     @Test
     void whenFullLatValid() {
         assertThat(isNameValid("first")).isTrue();
