70265ed9feaa0d3241d3ee88a0d4a0ace5365bcc
lenarsharipov <lenarsharipov@gmail.com>
2022-09-07 09:49:47 +0300

    update - 07.09.2022

--- src/ru/job4j/Main.java
+++ src/ru/job4j/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("20th of June, 2022");
+        System.out.println("привет!");
     }
 }
